6ac68638e730a696d5d2efd3d177fb051ac635ca
Rszooc <cursoszudemy@gmail.com>
2021-08-05 17:20:14 -0500

    primer paso

---
+++ src/scope/global.js
@@ -0,0 +1,17 @@
+var hello = "Hello";
+var hello = "Hello +"; // Mala practica
+let world = 'Hello World'; 
+let world = 'Hello World +'; //No se puede
+const helloWorld = 'Hello World';
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+anotherFunction();
+
+const helloWorld = () => {
+    globalVar = 
+}
+
